b9b291f3cd5e60397380aa7c75bf98b602b87323
eduardawild <eduarda.d.wild@gmail.com>
2015-05-20 11:54:42 -0300

    últimas versões

---
+++ Programação III/ShowdoMilhaoEduardaWild/src/modelo/JogoCompleto.java
@@ -0,0 +1,60 @@
+package modelo;
+
+public class JogoCompleto {
+
+    private Jogador jogador;
+    private Integer pular;
+    private Integer cartas;
+    private Integer placas;
+    private Double ganhos;
+
+    public JogoCompleto() 
+    {
+        this.jogador = null;
+        this.pular = 1;
+        this.cartas = 1;
+        this.pular = 1;
+        this.ganhos = 0.0;
+    }
+
+    public Jogador getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    public Integer getPular() {
+        return pular;
+    }
+
+    public void setPular(Integer pular) {
+        this.pular = pular;
+    }
+
+    public Integer getCartas() {
+        return cartas;
+    }
+
+    public void setCartas(Integer cartas) {
+        this.cartas = cartas;
+    }
+
+    public Integer getPlacas() {
+        return placas;
+    }
+
+    public void setPlacas(Integer placas) {
+        this.placas = placas;
+    }
+
+    public Double getGanhos() {
+        return ganhos;
+    }
+
+    public void setGanhos(Double ganhos) {
+        this.ganhos = ganhos;
+    }
+
+}
